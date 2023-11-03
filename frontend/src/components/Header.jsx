3731e81fb2f15afbcbefe9423ca6334fcebd750f
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-11-03 22:58:58 +0100

    bootstrap, navbar, footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { FaUser, FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">GrowShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link hfer="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link hfer="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
