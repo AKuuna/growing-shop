3648e6b04f79d4db3f9dbacd977ee6a5430f456a
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-11-14 22:37:25 +0100

    images, products homescreen, cards

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Scindapsus Moonlight Silver",
+    image: "/images/plant-_0000_scindapsus-moonlight-silver.jpg",
+    description:
+      "The Scindapsus is a rare, elegant plant in the Aracae family. The 'Moonlight Sliver' variety is notable for the silvery sheen that covers its oval-shaped foliage. If taken care of properly, this plant can become a large, lush, tropical showstopper.",
+    family: "Scindapsus",
+    category: "Trailing",
+    price: 99.99,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Scindapsus Exotica",
+    image: "/images/plant-_0001_scindapsus-exotica.jpg",
+    description:
+      "Scindapsus Pictus 'Exotica' is a rare trailing and vining plant famous about plant collectors. It has a striking light green foliage with silvery, spotty leaves but with a green streak in the middle of the leaves.",
+    family: "Scindapsus",
+    category: "Trailing",
+    price: 19.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Scindapsus Moonlight",
+    image: "/images/plant-_0002_scindapsus-moon.jpg",
+    description:
+      "The Scindapsus is a rare, elegant plant in the Aracae family. The 'Moonlight' variety is notable for its oval-shaped foliage. If taken care of properly, this plant can become a large, lush, tropical showstopper.",
+    family: "Scindapsus",
+    category: "Trailing",
+    price: 79.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Epipremnum Pearls & Jade",
+    image: "/images/plant-_0003_epi-pearl-jade.jpg",
+    description:
+      "Known as the perfect beginner-friendly plant, almost all houseplant lovers started with at least one Pothos before expanding their collection. Jade Pothos is not as common but is still widely available.",
+    family: "Pothos",
+    category: "Trailing",
+    price: 13.99,
+    countInStock: 21,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Epipremnum N'joy",
+    image: "/images/plant-_0004_epi-njoy.jpg",
+    description:
+      "N’joy pothos are beautiful, delicate pothos that are beloved for their variegated leaves and vining growth habit.",
+    family: "Pothos",
+    category: "Trailing",
+    price: 9.99,
+    countInStock: 17,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Scindapsus Treubie",
+    image: "/images/plant-_0005_scindapsus-treubie.jpg",
+    description:
+      "Scindapsus Treubie, more commonly known as Devil's Ivy, is a stunning large, leafy green houseplant. This glorious climbing variety is planted with a central moss pole to aid the growth and height from this fabulous indoor plant.",
+    family: "Scindapsus",
+    category: "Trailing",
+    price: 15.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
